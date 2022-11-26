620964dc1aeff1ef646ee45391ceeea6d78c1a3e
Anil2421 <akumar2421@gmail.com>
2022-11-26 13:02:26 +0530

    Add string declaration prg

---
+++ Beginner-Intro2.go
@@ -0,0 +1,13 @@
+/*
+This program will print my address on the console
+In order to build this program as executable:
+go build Beginner-Intro2.go
+
+To Run this program: go run Beginner-Intro2.go
+*/
+package main
+import "fmt"
+func main(){
+	fmt.Print("My Address is:")
+	fmt.Println("\nStreet-36, XYZ, U.P.")
+}
